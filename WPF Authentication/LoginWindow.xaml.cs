e55bdf51f988cf1238381c467b9505a23b020ebe
Aziz Khushvaktov <xua57570@gmail.com>
2024-07-13 20:48:26 +0500

    Add project files.

---
+++ WPF Authentication/LoginWindow.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Authentication
+{
+
+    public partial class LoginWindow : UserControl
+    {
+        private const string CredentialFilePath = "credentials.txt";
+        private readonly MainWindow _mainWindow;
+        private string connectionString = string.Empty;
+
+        public LoginWindow(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            
+            connectionString = ConfigurationManager.ConnectionStrings["UserDataBaseConnectionString"].ConnectionString;
+
+             _mainWindow = mainWindow;
+            LoadCredentials();
+        }
+
+        private void TogglePasswordVisibility_Click(object sender, RoutedEventArgs e)
+        {   
+            if (PasswordBox.Visibility == Visibility.Visible)
+            {
+                PasswordBox.Visibility = Visibility.Collapsed;
+                VisiblePasswordTextBox.Visibility = Visibility.Visible;
+                VisiblePasswordTextBox.Text = PasswordBox.Password;
+            }
+            else
+            {
+                PasswordBox.Visibility = Visibility.Visible;
+                VisiblePasswordTextBox.Visibility = Visibility.Collapsed;
+                PasswordBox.Password = VisiblePasswordTextBox.Text;
+            }
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = (PasswordBox.Password.Length > 0) ? PasswordBox.Password : VisiblePasswordTextBox.Text;
+
+            if (RememberMeCheckBox.IsChecked == true)
+            {
+                File.WriteAllText(CredentialFilePath, $"{username}\n{password}");
+            }
+
+            string userNameInFile = string.Empty;
+            string passwordInFile = string.Empty;
+
+            if (File.Exists(CredentialFilePath))
+            {
+                var lines = File.ReadAllLines(CredentialFilePath);
+                if (lines.Length == 2)
+                {
+                    userNameInFile = lines[0];
+                    passwordInFile = lines[1];
+                }
+            }
+            
+           if (LoginUser(username, password))
+           {
+               MessageBox.Show("Login successfull!", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+           } else
+            {
+                MessageBox.Show("Invalid username or password", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            _mainWindow.ShowRegistration();
+        }
+
+        /// <summary>
+        /// Loads user's information if any data available
+        /// </summary>
+        private void LoadCredentials()
+        {
+            if (File.Exists(CredentialFilePath))
+            {
+                var lines = File.ReadAllLines(CredentialFilePath);
+                if (lines.Length == 2)
+                {
+                    UsernameTextBox.Text = lines[0];
+                    PasswordBox.Password = lines[1];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets username and password that a user entered, hashs the password with Salt in DB and compares with the one in DB
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns>Returns true if the users already registered</returns>
+        private bool LoginUser(string username, string password)
+        {
+            using(SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand("SELECT PasswordHash, Salt FROM Users WHERE Username = @Username", sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@Username", username);
+
+                using(var reader = sqlCommand.ExecuteReader())
+                {
+                    if(reader.Read())
+                    {
+                        var storedPasswordHash = reader["PasswordHash"].ToString();
+                        var storedSalt = reader["Salt"].ToString();
+
+                        var passwordHash = HashPassword(password, storedSalt);
+                        return (storedPasswordHash == passwordHash) ? true : false;
+                    } 
+
+                    return false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Hashes a password with a given salt using SHA-256.
+        /// </summary>
+        /// <param name="password">The plain text password to be hashed.</param>
+        /// <param name="salt">The salt to be added to the password before hashing.</param>
+        /// <returns>A base64 encoded string representing the hashed password.</returns>
+        private string HashPassword(string password, string salt)
+         {
+            using (var sha256 = SHA256.Create())
+            {
+                var saltedPassword = password + salt;
+                var saltedPasswordBytes = Encoding.UTF8.GetBytes(saltedPassword);
+                var hashBytes = sha256.ComputeHash(saltedPasswordBytes);
+                return Convert.ToBase64String(hashBytes);
+            }
+         }
+    }
+}
---
+++ WPF Authentication/RegistrationWindow.xaml.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace WPF_Authentication
+{
+    public partial class RegistrationWindow : UserControl
+    {
+        private readonly MainWindow _mainWindow;
+        private string password = string.Empty;
+        private string connectionString = string.Empty;
+
+        public RegistrationWindow(MainWindow mainWindow)
+        {
+            InitializeComponent();
+
+            connectionString = ConfigurationManager.ConnectionStrings["UserDataBaseConnectionString"].ConnectionString;
+
+            _mainWindow = mainWindow;
+        }
+
+        private void TogglePasswordVisibility_Click(object sender, RoutedEventArgs e)
+        {
+            if (PasswordBox_Reg.Visibility == Visibility.Visible)
+            {   
+                password = PasswordBox_Reg.Password;
+
+                PasswordBox_Reg.Visibility = Visibility.Collapsed;
+                VisiblePasswordTextBox.Visibility = Visibility.Visible;
+                VisiblePasswordTextBox.Text = PasswordBox_Reg.Password;
+
+            }
+            else
+            {
+                password = VisiblePasswordTextBox.Text;
+
+                PasswordBox_Reg.Visibility = Visibility.Visible;
+                VisiblePasswordTextBox.Visibility = Visibility.Collapsed;
+                PasswordBox_Reg.Password = VisiblePasswordTextBox.Text;
+            }
+        }
+
+        private void ToggleConfirmPasswordVisibility_Click(object sender, RoutedEventArgs e)
+        {
+            if (ConfirmPasswordBox.Visibility == Visibility.Visible)
+            {
+                ConfirmPasswordBox.Visibility = Visibility.Collapsed;
+                VisibleConfirmPasswordTextBox.Visibility = Visibility.Visible;
+                VisibleConfirmPasswordTextBox.Text = ConfirmPasswordBox.Password;
+            }
+            else
+            {
+                ConfirmPasswordBox.Visibility = Visibility.Visible;
+                VisibleConfirmPasswordTextBox.Visibility = Visibility.Collapsed;
+                ConfirmPasswordBox.Password = VisibleConfirmPasswordTextBox.Text;
+            }
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string confirmPassword = (ConfirmPasswordBox.Password.Length > 0) ? ConfirmPasswordBox.Password : VisibleConfirmPasswordTextBox.Text;
+
+            password = (PasswordBox_Reg.Password.Length > 0) ? PasswordBox_Reg.Password : VisiblePasswordTextBox.Text;
+
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Passwords do not match.");
+                return;
+            }
+
+            if (ValidateCredentials(username, password))
+            {
+               if(RegisterUser(username, password))
+               {
+                    MessageBox.Show("Registered successfully!", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    BackToLoginPageWithPopulatedCredentials(username, password);
+               }  
+            }
+
+        }
+
+        /// <summary>
+        /// Saves a users to DB
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns>If registration is succeed, then return true</returns>
+        private bool RegisterUser(string username, string password)
+        {
+             using(SqlConnection sqlConnection = new SqlConnection(connectionString))
+             {
+                sqlConnection.Open();
+                
+                var salt = GenerateSalt();
+                var passwordHash = HashPassword(password, salt);
+
+                SqlCommand sqlCommand = new SqlCommand("INSERT INTO Users (Username, PasswordHash, Salt) VALUES (@Username, @PasswordHash, @Salt)", sqlConnection);
+
+                sqlCommand.Parameters.AddWithValue("@Username", username);
+                sqlCommand.Parameters.AddWithValue("@PasswordHash", passwordHash);
+                sqlCommand.Parameters.AddWithValue("@Salt", salt);
+
+                try
+                {
+                    sqlCommand.ExecuteNonQuery();
+                    return true; 
+
+                } catch(Exception ex)
+                {   
+                    MessageBox.Show($"{ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);MessageBox.Show(ex.Message);
+                    return false;
+                }
+             }
+        }
+
+        public void BackToLogin_Click(object sender, EventArgs e)
+        {
+            _mainWindow.ShowLogin();
+        }
+
+        /// <summary>
+        /// Generates a random salt value to be used for password hashing.
+        /// </summary>
+        /// <returns>A base64 encoded string representing the generated salt.</returns>
+        private string GenerateSalt()
+        {
+            var rng = new RNGCryptoServiceProvider();
+            var saltBytes = new byte[32];
+            rng.GetBytes(saltBytes);
+
+            return Convert.ToBase64String(saltBytes);
+        }
+
+         /// <summary>
+        /// Hashes a password with a given salt using SHA-256.
+        /// </summary>
+        /// <param name="password">The plain text password to be hashed.</param>
+        /// <param name="salt">The salt to be added to the password before hashing.</param>
+        /// <returns>A base64 encoded string representing the hashed password.</returns>
+        private string HashPassword(string password, string salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var saltedPassword = password + salt;
+                var saltedPasswordBytes = Encoding.UTF8.GetBytes(saltedPassword);
+                var hashBytes = sha256.ComputeHash(saltedPasswordBytes);
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+
+        /// <summary>
+        /// Validate username and password
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns>If everything is OK, then return true</returns>
+        private bool ValidateCredentials(string username, string password)
+        {
+            // Regex pattern for username: at least 8 characters
+            string usernamePattern = @".{8,}";
+            // Regex pattern for password: at least 8 characters, one uppercase letter, and one digit
+            string passwordPattern = @"^(?=.*[A-Z])(?=.*\d).{8,}$";
+
+            // Validate username
+            if (!Regex.IsMatch(username, usernamePattern))
+            {
+                MessageBox.Show("Username must be at least 8 characters!");
+                return false;
+            }
+
+            // Validate password
+            if (!Regex.IsMatch(password, passwordPattern))
+            {
+                MessageBox.Show("Password must be at least 8 characters, contain an uppercase character, and a digit!");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void BackToLoginPageWithPopulatedCredentials(string username, string password)
+        {
+             // Open login window with populated credentials
+            _mainWindow.ShowLogin();
+            ((LoginWindow)_mainWindow.MainContentControl.Content).UsernameTextBox.Text = username;
+            ((LoginWindow)_mainWindow.MainContentControl.Content).PasswordBox.Password = password;
+        }
+    }
+}
